a31c06d64f6227befdfc88d9b39f840fec44498c
riguang <102936778@qq.com>
2017-12-21 18:45:04 +0800

    爬虫的话和报错处理

--- src/utils/Spider.js
+++ src/utils/Spider.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer'),
     fs = require('fs'),
     path = require('path'),
-    request = require('request');
+    request = require('request'),
+    uuid = require('node-uuid');
 const { timeout } = require('../../tools/tools.js');
 
 const downloadImage = async(src, dest, callback) => {
@@ -17,83 +18,157 @@ const downloadImage = async(src, dest, callback) => {
 };
 
 const spider = async(wxName) => {
+    let pages = new Array(wxName.length);
     puppeteer.launch({ headless: false }).then(async browser => {
-        let page = await browser.newPage();
-        const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36';
-        await page.setUserAgent(ua);
-        //await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 1 });
-        await page.goto(`http://weixin.sogou.com/weixin?type=1&s_from=input&query=${encodeURIComponent(wxName)}`);
-        await timeout(2000);
-        let aTags = await page.evaluate(() => {
-            let as = [...document.querySelectorAll('a[uigs="account_name_0"]')];
-            return as.map((a) => {
-                return {
-                    href: a.href.trim(),
-                    name: a.text
+        for (let ii = 0; ii < wxName.length; ii++) {
+            try {
+                console.log(`正在爬取${wxName[ii]}...`);
+                pages[ii] = {
+                        a: await browser.newPage(),
+                        b: await browser.newPage(),
+                        c: await browser.newPage()
+                    }
+                    //const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36';
+                    //await page.setUserAgent(ua);
+                    //await page.setViewport({ width: 1200, height: 800, deviceScaleFactor: 1 });
+                await pages[ii].a.goto(`http://weixin.sogou.com/weixin?type=1&s_from=input&query=${encodeURIComponent(wxName[ii])}`);
+                await timeout(Math.floor(Math.random() * 10000));
+                let aTags = await pages[ii].a.evaluate(() => {
+                    window.scrollBy(0, window.innerHeight); //模拟滚动视图
+                    let as = [...document.querySelectorAll('a[uigs="account_name_0"]')];
+                    if (as.length) {
+                        return as.map((a) => {
+                            return {
+                                href: a.href.trim(),
+                                name: a.text
+                            }
+                        });
+                    } else {
+                        return false;
+                    }
+                });
+                console.log("page1:" + ii, aTags || `${wxName[ii]}'未找到'`);
+                //await page.pdf({ path: `./data/es6-pdf/${aTags[0].name}1.pdf` });
+                //await page.close();
+                //获取公众号页面
+                //page2 = await browser.newPage();
+                if (!aTags) {
+                    await pages[ii].a.close();
+                    await pages[ii].b.close();
+                    await pages[ii].c.close();
+                    continue;
                 }
-            });
-        });
-        console.log("aTags", aTags);
-        //await page.pdf({ path: `./data/es6-pdf/${aTags[0].name}1.pdf` });
-        await page.close();
-        //获取公众号页面
-        page = await browser.newPage();
-        let PublicNumber = aTags[0];
-        await page.goto(PublicNumber.href);
-        // await timeout(2000);
-        let firstDynamic = await page.evaluate(() => {
-            let as = [...document.querySelectorAll('#history .weui_msg_card .weui_media_title')];
-            return as.map((a) => {
-                return {
-                    href: a.outerHTML.trim(),
-                    name: a.innerText
+                let PublicNumber = aTags[0];
+                await pages[ii].b.goto(PublicNumber.href);
+                await timeout(Math.floor(Math.random() * 10000));
+                let firstDynamic = await pages[ii].b.evaluate(() => {
+                    window.scrollBy(0, window.innerHeight); //模拟滚动视图
+                    let as = [...document.querySelectorAll('#history .weui_msg_card .weui_media_title')];
+                    if (as.length) {
+                        return as.map((a) => {
+                            return {
+                                href: a.outerHTML.trim(),
+                                name: a.innerText
+                            }
+                        })[0];
+                    } else {
+                        return false;
+                    }
+                });
+                console.log('page2:' + ii, firstDynamic || `${wxName[ii]}'未找到'`);
+                if (!firstDynamic) {
+                    await pages[ii].a.close();
+                    await pages[ii].b.close();
+                    await pages[ii].c.close();
+                    continue;
                 }
-            })[0];
-        });
-        console.log('firstDynamic', firstDynamic);
-        const strLeft = '<h4 class="weui_media_title" hrefs="';
-        const strRight = '">';
-        let leftPosition = firstDynamic.href.indexOf(strLeft) + strLeft.length;
-        let rightPosition = firstDynamic.href.indexOf(strRight);
-        firstDynamic.href = ('http://mp.weixin.qq.com' + firstDynamic.href.substring(leftPosition, rightPosition)).replace(/\amp;/g, "");
-        console.log('href:', firstDynamic.href);
-        //await page.pdf({ path: `./data/es6-pdf/${PublicNumber.name}2.pdf` });
-        await page.close();
-        //获取公众号最新一条动态
-        page = await browser.newPage();
-        await page.goto(firstDynamic.href);
-        //await timeout(2000);
-        //await page.pdf({ path: `./data/es6-pdf/${PublicNumber.name}3.pdf` });
-        let getContent = await page.evaluate(() => { //取网页源码
-            let HTML = document.getElementById('img-content').innerHTML;
-            let imgs = [...document.querySelectorAll("img[data-src]")].map((v, i) => {
-                return v.getAttribute('data-src');
-            })
-            return {
-                html: HTML,
-                imgs: imgs
-            };
-        });
-        let showImgJs = `<script>const imgDOM=[...document.querySelectorAll("img[data-src]")];imgDOM.map((v)=>{v.setAttribute("src",v.getAttribute("data-src"));});</script>`;
-        fs.writeFileSync(`./data/wx-html/${wxName}.html`, getContent.html + showImgJs);
-        // getContent.imgs.map((v, i) => { //下载图片
-        //     downloadImage(v, `./wx-img/${Math.random()*1000}.jpg`, (err, dest) => {
-        //         if (err) {
-        //             console.log(err);
-        //         }
-        //         if (dest) {
-        //             console.log(dest);
-        //         }
-        //     });
-        // });
-        await page.close();
+                const strLeft = '<h4 class="weui_media_title" hrefs="';
+                const strRight = '">';
+                let leftPosition = firstDynamic.href.indexOf(strLeft) + strLeft.length;
+                let rightPosition = firstDynamic.href.indexOf(strRight);
+                firstDynamic.href = ('http://mp.weixin.qq.com' + firstDynamic.href.substring(leftPosition, rightPosition)).replace(/\amp;/g, "");
+                console.log('href:', firstDynamic.href);
+                //await page.pdf({ path: `./data/es6-pdf/${PublicNumber.name}2.pdf` });
+                //await page2.close();
+                //获取公众号最新一条动态
+                //page3 = await browser.newPage();
+                await pages[ii].c.goto(firstDynamic.href);
+                await timeout(Math.floor(Math.random() * 10000));
+                //await page.pdf({ path: `./data/es6-pdf/${PublicNumber.name}3.pdf` });
+                let getContent = await pages[ii].c.evaluate(() => { //取网页源码
+                    window.scrollBy(0, window.innerHeight); //模拟滚动视图
+                    let imgs = [...document.querySelectorAll("img[data-src]")].map((v, i) => {
+                        v.setAttribute('onerror', "javascript:this.src=this.getAttribute('data-src');");
+                        // let imgID = uuid.v4().replace(/\-/g, "");
+                        let imgID = Math.floor(Math.random() * 100000000).toString() + Date.now().toString();
+                        let imgSrc = v.getAttribute('data-src');
+                        let suffix = imgSrc.substr(imgSrc.indexOf('wx_fmt=')).replace('wx_fmt=', "");
+                        switch (suffix) {
+                            case 'png':
+                                break;
+                            case 'jpg':
+                                break;
+                            case 'jpeg':
+                                break;
+                            case 'gif':
+                                break;
+                            default:
+                                suffix = 'gif';
+                        }
+                        let imgName = `${imgID}.${suffix}`;
+                        v.setAttribute("src", `../wx-img/${imgName}`);
+                        return {
+                            src: imgSrc,
+                            name: imgName
+                        };
+                    });
+                    //js_article
+                    //page-content
+                    document.getElementById('js_pc_qr_code').outerHTML = ""; //清除公众号二维码
+                    [...document.querySelectorAll('img')].map((v) => { v.classList.remove("img_loading"); }); //清除img_loading class
+                    document.getElementsByClassName('reward_qrcode_img_wrp')[0].outerHTML = ""; //清除reward_qrcode_img_wrp
+                    let HTML = document.getElementById('js_article').outerHTML;
+                    return {
+                        html: HTML,
+                        imgs: imgs
+                    };
+                });
+                console.log('page3', getContent);
+                //let showImgJs = `<script>const imgDOM=[...document.querySelectorAll("img[data-src]")];imgDOM.map((v)=>{v.setAttribute("src",v.getAttribute("data-src"));});</script>`;
+                let css = '<script>document.head.innerHTML=\'<link href="../wx-css/style1.css" rel="stylesheet" type="text/css"><link href="../wx-css/style2.css" rel="stylesheet" type="text/css"><link href="../wx-css/style3.css" rel="stylesheet" type="text/css">\';</script>';
+                await fs.writeFileSync(`./data/wx-html/${wxName[ii]}.html`, css + getContent.html);
+                await getContent.imgs.map((v, i) => { //下载图片
+                    if (v.src.indexOf('https') != -1) {
+                        downloadImage(v.src, `./data/wx-img/${v.name}`, (err, dest) => {
+                            console.log(v.src);
+                            if (err) {
+                                console.log(err);
+                            }
+                            if (dest) {
+                                console.log(dest);
+                            }
+                        });
+                    }
+                });
+                await pages[ii].a.close();
+                await pages[ii].b.close();
+                await pages[ii].c.close();
+            } catch (error) {
+                await pages[ii].a.close();
+                await pages[ii].b.close();
+                await pages[ii].c.close();
+                console.log(error);
+            }
+        }
+
         await browser.close();
     });
 }
 const moreSpider = async(wxArr) => {
-    wxArr.map((val) => {
-        spider(val);
-    });
+    // wxArr.map((val) => {
+    //     spider(val);
+    // });
+    spider(wxArr);
 };
 
 module.exports = moreSpider;
--- src/wx.js
+++ src/wx.js
@@ -1,11 +1,37 @@
 const moreSpider = require('./utils/Spider.js');
 const wxArr = [
-    // 'jackmafoundation',
-    // '电影工厂',
-    // 'adnlb11111111',
-    // 'mayundengshi8899',
-    // 'mxd2tencent',
-    'mxd2helper'
+    'keso怎么看',
+    '老道消息',
+    '小道消息',
+    'caoz',
+    '42章经',
+    '硅谷程序汪',
+    '邹大湿',
+    '新智元',
+    '量子位',
+    '商业人物',
+    '猫财经',
+    'GPLP',
+    'GQ实验室',
+    'AI财经社',
+    '每日人物',
+    'autocarweekly',
+    'KnowingAI知智',
+    '造就',
+    '锌财经',
+    '李开复',
+    '盛盛GO',
+    '雷军',
+    '歪理邪说',
+    '华尔街日报·派',
+    'RayGeek',
+    '魏布斯',
+    '少数派',
+    '雷帝触网',
+    '可能吧',
+    '机器之心',
+    'yivian',
+    'MAC玩儿法'
 ];
 
 //moreSpider(wxArr);
